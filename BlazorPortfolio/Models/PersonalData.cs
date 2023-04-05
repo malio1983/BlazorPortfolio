ae20e4981d3207fd24585e8fc6255b091b452a16
Malio-HP\malio <malio1983@gmail.com>
2023-04-05 19:15:25 +0200

    Added order by date to Experience.

--- BlazorPortfolio/Models/PersonalData.cs
+++ BlazorPortfolio/Models/PersonalData.cs
@@ -3,22 +3,24 @@
 
     public class PersonalData
     {
+        private List<Experience> experiences;
+
         public PersonalData()
         {
             SocialMedias = new List<SocialMedia>();
             Skills = new List<Skill>();
-            Experiences = new List<Experience>();
+            experiences = new List<Experience>();
             Projects = new List<Project>();
         }
 
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty; 
+        public string Description { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public List<SocialMedia> SocialMedias { get; set; }
         public List<Skill> Skills { get; set; }
-        public List<Experience> Experiences { get; set; }
+        public List<Experience> Experiences { get => experiences.OrderByDescending(x=>x.From).ThenBy(x=>x.To).ToList(); set => experiences = value; }
         public List<Project> Projects { get; set; }
     }
 
@@ -40,8 +42,8 @@
     public class Experience 
     {
         
-        public string From { get; set; } = string.Empty;
-        public string To { get; set; } = string.Empty;
+        public DateTime From { get; set; } 
+        public DateTime To { get; set; } 
         public string Company { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
     }
